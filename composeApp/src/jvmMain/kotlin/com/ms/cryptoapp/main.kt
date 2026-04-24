54128acf11b0079d26c92765d75a40cde260ca95
mukesh <mukesh.appdev16@gmail.com>
2026-04-24 19:17:28 +0530

    Crypto base code

---
+++ composeApp/src/jvmMain/kotlin/com/ms/cryptoapp/main.kt
@@ -0,0 +1,13 @@
+package com.ms.cryptoapp
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CryptoApp",
+    ) {
+        App()
+    }
+}
